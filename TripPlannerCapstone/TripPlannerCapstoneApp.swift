9a46193fe03f9cf92b5e5c791a4583e1ff86c0c5
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-05-26 16:55:31 -0500

    Initial Commit

---
+++ TripPlannerCapstone/TripPlannerCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerCapstoneApp.swift
+//  TripPlannerCapstone
+//
+//  Created by Renato Oliveira Fraga on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
